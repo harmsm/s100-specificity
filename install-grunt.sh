5132dcdb32be52c40422ac20c151587c8119d1cf
Mike Harms <harmsm@gmail.com>
2018-01-29 08:52:38 -0800

    initial commit

---
+++ install-grunt.sh
@@ -0,0 +1,12 @@
+npm init
+npm install grunt-cli
+npm install grunt --save-dev
+npm install grunt-contrib-qunit --save-dev
+npm install grunt-contrib-jshint --save-dev
+npm install grunt-contrib-cssmin --save-dev
+npm install grunt-contrib-uglify --save-dev
+npm install grunt-contrib-watch --save-dev
+npm install grunt-sass --save-dev
+npm install grunt-contrib-connect --save-dev
+npm install grunt-autoprefixer --save-dev
+npm install grunt-zip --save-dev
